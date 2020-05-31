87aef14418468b27da354cff499f759fdc2821d6
Jakub Havelka <jakub@havelka.net>
2020-05-31 23:59:22 +0200

    More size optimizations

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue'
 
 import {router} from '@/router';
 import App from "./views/App.vue";
-import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
@@ -12,9 +11,7 @@ export class DataPlatform {
     constructor(selector: string) {
         new Vue({
             router,
-            i18n,
             render: (createElement) => createElement(App),
-
         }).$mount(selector)
     }
 }
