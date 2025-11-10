309e960ec4047983e3c29010ff459efe983a0f51
HamAndRock <jakub@havelka.net>
2025-11-10 17:40:55 +0100

    feat: image scaling on phone

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,7 +13,7 @@ interface HeaderProps {
     changeLanguage: (lang: Language) => void
 }
 
-export const Header = ({ changeLanguage, lang } : HeaderProps) => (
+export const Header = ({ changeLanguage, lang }: HeaderProps) => (
     <StyledHeader>
         <Navbar>
             <Logo />
@@ -73,43 +73,46 @@ const LanguageContainer = styled.div`
 `;
 
 const Portrait = styled.div`
-  position: absolute;
-  filter: drop-shadow(14px 0px 0px #C7D0F0);
-  right: 0;
-  bottom: -7px;
+  img {
+    position: absolute;
+    filter: drop-shadow(14px 0px 0px #C7D0F0);
+    right: 0;
+    bottom: 0;
 
-  @media (max-width: 1300px) {
-    width: 550px;
-  }
+    @media (max-width: 1300px) {
+      width: 550px;
+      height: auto;
+    }
 
-  //@media (max-width: 800px) {
-  //  width: 350px;
-  //  left: 0;
-  //  right: unset;
-  //  border-bottom-left-radius: 100px;
-  //}
+    //@media (max-width: 800px) {
+    //  width: 350px;
+    //  left: 0;
+    //  right: unset;
+    //  border-bottom-left-radius: 100px;
+    //}
 
+    @media (max-width: 800px) {
+      left: 0;
+      bottom: -7px;
+      width: 350px;
+      border-bottom-left-radius: 100px;
+      right: unset;
+      //clip-path: circle(123.45% at 61.73% -22.03%);
+      //clip-path: polygon(58% 93%, 42% 91%, 32% 88%, 22% 83%, 13% 76%, 9% 66%, 5% 54%, 4% 43%, 3% 32%, 3% 23%, 34% 9%, 72% 10%, 93% 26%, 94% 78%, 95% 89%);
+      //clip-path: polygon(45.14% 97.95%, 24.00% 97.49%, 14.29% 94.49%, 8.85% 89.51%, 5% 84.99%, 1.29% 77.15%, 0.71% 59.89%, 0.57% 45.17%, 1.29% 34.79%, 1.01% 24.24%, 1.14% -8.66%, 53.14% -9.83%, 143.57% -14.89%, 157.14% 31.53%, 157.86% 97.98%);
+      clip-path: polygon(45.14% 97.95%, 24% 97.49%, 18% 95.42%, 13.14% 93.23%, 8.71% 89.64%, 5% 84.9%, 2.14% 78.79%, 0.57% 72.43%, 1.01% 24.24%, 1.14% -8.66%, 53.14% -9.83%, 143.57% -14.89%, 157.14% 31.53%, 157.86% 97.98%);
 
-  @media (max-width: 800px) {
-    left: 0;
-    width: 350px;
-    border-bottom-left-radius: 100px;
-    right: unset;
-    //clip-path: circle(123.45% at 61.73% -22.03%);
-    //clip-path: polygon(58% 93%, 42% 91%, 32% 88%, 22% 83%, 13% 76%, 9% 66%, 5% 54%, 4% 43%, 3% 32%, 3% 23%, 34% 9%, 72% 10%, 93% 26%, 94% 78%, 95% 89%);
-    //clip-path: polygon(45.14% 97.95%, 24.00% 97.49%, 14.29% 94.49%, 8.85% 89.51%, 5% 84.99%, 1.29% 77.15%, 0.71% 59.89%, 0.57% 45.17%, 1.29% 34.79%, 1.01% 24.24%, 1.14% -8.66%, 53.14% -9.83%, 143.57% -14.89%, 157.14% 31.53%, 157.86% 97.98%);
-    clip-path: polygon(45.14% 97.95%, 24% 97.49%, 18% 95.42%, 13.14% 93.23%, 8.71% 89.64%, 5% 84.9%, 2.14% 78.79%, 0.57% 72.43%, 1.01% 24.24%, 1.14% -8.66%, 53.14% -9.83%, 143.57% -14.89%, 157.14% 31.53%, 157.86% 97.98%);
-  
-    //width: 100vw;
-    //left: unset;
-    //
-    //
-    //border-bottom-left-radius: 392px;
-    //border-left: 165px solid white;
-    //border-bottom: 30px solid white;
-    //
-    //position: absolute;
-    //bottom: -40px;
+      //width: 100vw;
+      //left: unset;
+      //
+      //
+      //border-bottom-left-radius: 392px;
+      //border-left: 165px solid white;
+      //border-bottom: 30px solid white;
+      //
+      //position: absolute;
+      //bottom: -40px;
+    }
   }
 `;
 const Lang = styled.div<{ active?: boolean }>`
